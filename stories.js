168aeb3e5f356c34b15482f93fa26f4dab8dd30e
zcjtshy <see.yim.13@ucl.ac.uk>
2017-03-13 08:16:34 +0000

    delete html comment on stories.js

--- stories.js
+++ stories.js
@@ -13,48 +13,48 @@ var stories = [
 		filter:'stress',
 		story: 'I had left my job due to severe stress and did not have a job to go to. I was very low and lacking in self-confidence. I started to make myself go for little local walks from my home in the middle of the city. In places where there were lots of people I felt lost, they all seemed full of purpose and direction compared to me. I didn\'t belong. Then I started to notice small pockets of nature that were managing to thrive in harsh conditions. Noticing and then photographing this nature in the urban environment gave me a focus and, looking back, I suppose was a kind of metaphor for hope in bleak surroundings. It was a kind of mindfulness that enabled me to connect again. This photograph is one of those I took during this time of a beautiful lilac blossom growing in a parking bay on the estate where I live. '
 	},
-	// {
-	// 	title: '',
-	// 	name: '',
-	// 	image: '',
-	// 	story: ''
-	// },
-	// {
-	// 	title: '',
-	// 	name: '',
-	// 	image: '',
-	// 	story: ''
-	// },
-	// {
-	// 	title: '',
-	// 	name: '',
-	// 	image: '',
-	// 	story: ''
-	// },
-	// {
-	// 	title: '',
-	// 	name: '',
-	// 	image: '',
-	// 	story: ''
-	// },
-	// {
-	// 	title: '',
-	// 	name: '',
-	// 	image: '',
-	// 	story: ''
-	// },
-	// {
-	// 	title: '',
-	// 	name: '',
-	// 	image: '',
-	// 	story: ''
-	// },
-	// {
-	// 	title: '',
-	// 	name: '',
-	// 	image: '',
-	// 	story: ''
-	// }
+	{
+		title: '',
+		name: '',
+		image: '',
+		story: ''
+	},
+	{
+		title: '',
+		name: '',
+		image: '',
+		story: ''
+	},
+	{
+		title: '',
+		name: '',
+		image: '',
+		story: ''
+	},
+	{
+		title: '',
+		name: '',
+		image: '',
+		story: ''
+	},
+	{
+		title: '',
+		name: '',
+		image: '',
+		story: ''
+	},
+	{
+		title: '',
+		name: '',
+		image: '',
+		story: ''
+	},
+	{
+		title: '',
+		name: '',
+		image: '',
+		story: ''
+	}
 ];
 
 function createStory(story) {
